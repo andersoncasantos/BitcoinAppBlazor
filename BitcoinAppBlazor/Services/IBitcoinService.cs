1e758b953d0faaf52eff85f770850f6964395bf4
andersoncasantos <andersonmtv07@gmail.com>
2023-10-05 18:32:34 -0300

    first commit

---
+++ BitcoinAppBlazor/Services/IBitcoinService.cs
@@ -0,0 +1,9 @@
+﻿using BitcoinAppBlazor.DTOs;
+
+namespace BitcoinAppBlazor.Services
+{
+    public interface IBitcoinService
+    {
+        Task<List<BitcoinDataDTO>> FindBy(DateTime startDate);
+    }
+}
